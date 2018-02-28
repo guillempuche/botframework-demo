4be30675b8d0a52a24be67a0ab93d56de5ba2f09
Guillem Puche <guillem7puche@gmail.com>
2018-02-28 18:42:04 +0100

    Azure CosmosDB

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const builder = require('botbuilder');
 const restify = require('restify');
-require('dotenv').config()
+require('dotenv').config();
+var azure = require('botbuilder-azure'); 
+var MongoClient = require('mongodb').MongoClient;
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -8,9 +10,27 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
+var documentDbOptions = {
+    host: 'botframework-demo.documents.azure.com', 
+    masterKey: 'cww3jgWeWDgKqRkdUdpmgOTa8RNLN01P9RLPgRKyUt44cmAHYIYen510vWJeu2sUOInTgTjjFCbQKDQrwf77Qg==', 
+    database: 'test',   
+    collection: 'test1'
+};
+
+// Create an instance of 'DocumentDBClient' passing in the connection configuration settings
+var docDbClient = new azure.DocumentDbClient(documentDbOptions);
+// Create an instance of 'AzureBotStorage' passing in the DocumentDBClient
+var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     // https://docs.microsoft.com/en-us/bot-framework/bot-service-troubleshoot-authentication-problems
+    
+    // Test on Web Chat Emulator Localhost
+    //appId: null,
+    //appPassword: null
+
+    // Deploy on Azure App Service
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
@@ -18,7 +38,8 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector)
+    .set('storage', cosmosStorage); // Register in-memory storage
 
 bot.dialog('/', [
     (session, results, next) => {
