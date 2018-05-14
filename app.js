41620286999048e07ebf83f7745d8ea0fbc42eeb
Guillem Puche <guillem7puche@gmail.com>
2018-05-14 17:30:26 +0200

    Añadido broadcasting. Y algunos arreglos.

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ if (process.env.NODE_ENV !== 'production') { // require('dotenv').config();
 };
 const builder = require('botbuilder');
 const restify = require('restify');
+const url = require('url');
 var MongoClient = require('mongodb').MongoClient;
+var savedAddress; // variable para guardar dirección para el broadcasting
 
 // URL de la base de datos (desactivada) de la nube de Azure
-const url = `mongodb://botframework-demo:${process.env.AZURE_COSMOSDB_MASTER_KEY}@botframework-demo.documents.azure.com:10255/?ssl=true`
+const urlDatabase = `mongodb://botframework-demo:${process.env.AZURE_COSMOSDB_MASTER_KEY}@botframework-demo.documents.azure.com:10255/?ssl=true`
 
 // Configurar servidor Restify
 var server = restify.createServer();
@@ -50,7 +52,7 @@ var bot = new builder.UniversalBot(connector);
  */ 
 const commonIntentDialog = new builder.IntentDialog({
     intentThreshold: 0.4
-})
+});
 
 /**
  * Función 'recognizer' para decir a Bot Framework cuál es el intento del mensaje del usuario
@@ -90,25 +92,84 @@ bot.use({
     // se executan las operaciones practicamente al mismo tiempo. Es decir, no
     // se ejecuta todo el codigo de de 'logMessage' sino que se ejecuta algunas linias
     // se ejecutan primero para cada objeto 'session' y despues se ejecuta otra linia.
-    // En 'log_database.js' se usa un delay para solucionarlo.
     send: function (session, next) { // IMPORTANTE: objeto 'session' es diferente que en mensaje recibido 
         // Filtrar el mensaje del objeto 'session' ya que entre los mensajes
         // del usuario o el bot hay informacion irrelevante 'conversationUpdate'
         if (session.type != "conversationUpdate") {
             session.messageWatchedFromBot = "sent";
+
             require('./lib/log_database').logMessage(session)
         }
         next();
-
     }
 });
 
 /**
- * Función 'Dialog' que interectua con el usuario.
- * Tiene el objetivo de testear nuevas funciones para el chat.
+ * Crear un 'endpoint' GET para empezar el dialogo proactivo.
+ * 
+ * Ejemplo en https://github.com/Microsoft/BotBuilder-Samples/tree/master/Node/core-proactiveMessages
  */
+server.get('/api/broadcastSample', function (req, res, next) {
+    // El método url.parse() toma una cadena URL, la analiza ydevuelve un objeto URL.
+    // 'url.parse(urlString[, parseQueryString[, slashesDenoteHost]])'
+    // urlString <string> - Es la cadena URL para analizar o 'parse'.
+    var query = url.parse(req.url, true).query;
+    // beginDialogAction(name: string, id: string, options?: IBeginDialogActionOptions)
+        // name: string - Unique name to assign the action.
+        // id: string - ID of the dialog to start.
+        // options - (Optional) options used to configure the action or pass additional params
+    // Más información en https://docs.botframework.com/en-us/node/builder/chat-reference/classes/_botbuilder_d_.intentdialog.html#begindialogaction
+    bot.beginDialog(savedAddress, "*:/broadcastDialog", {
+        dialogArgs: {
+            broadcastMessage: query.message
+        }
+    });
+    //require('./lib/broadcast').startProactiveDialog(savedAddress, query.message);
+    res.send('triggered');
+    next();
+});
+
+
 bot.dialog('/', [
     (session, results, next) => {
+        session.send("Inici");
+    }
+]).reloadAction("Inicio", {
+    matches: [/^inicio$/i]
+});
+
+/**
+ * Diálogo que envia al usuario el mensaje que ha recibido
+ * via endpoint '/api/broadcastSample'.
+ */
+bot.dialog('/broadcastDialog', [
+    (session, args) => {
+        session.send(`[BROADCAST] ${args.dialogArgs.broadcastMessage}`)
+        session.endDialog("[BROADCAST] Fi del 'broadcastDialog'");
+    }
+]);
+
+/**
+ * Cuando se activa 'customAction', la opción 'onSelectAction'
+ * puede procesar la solicitud sin presionar nuevos cuadros de diálogo
+ * en la pila. Una vez que se completa la acción, el control vuelve al
+ * diálogo que está en la parte superior de la pila y el bot puede continuar.
+ */
+bot.customAction({
+    matches: /^rss$/i,
+    onSelectAction: (session, args, next) => {
+        savedAddress = session.message.address;
+        const message = 'http://localhost:' + server.address().port + "/api/broadcastSample";
+
+        session.send("Informació útil pel Broadcast");
+        session.send(`URL: ${message}`);
+    }
+});
+
+bot.dialog('/nombre', [
+    (session, results, next) => {
+        savedAddress = session.message.address;
+        
         if (session.conversationData.name == undefined) {
             session.send("Hola i benvingut! Jo sóc l'APU.");
             builder.Prompts.text(session, "I tu com et dius?");
@@ -118,7 +179,7 @@ bot.dialog('/', [
         }
     },
     (session, results) => {
-        if (results.response) session.conversationData.name = results.response;
+        if (results && results.response) session.conversationData.name = results.response;
         session.beginDialog('menu');
     },
     (session) => {
@@ -126,6 +187,8 @@ bot.dialog('/', [
     }
 ]).triggerAction({
     matches: [/^hola$/i, /^ei$/i]
+}).cancelAction('cancelNombre', "Ok, pararé el dialeg.", {
+    matches: /^cancelar$|^cancel$|^cancel.*order/i
 });
 
 bot.dialog('menu', [
@@ -133,7 +196,7 @@ bot.dialog('menu', [
         builder.Prompts.choice(session, `Què voldries saber ${session.conversationData.name}?`, 'UPC|ETSETB', { listStyle: builder.ListStyle.button })
     },
     (session, results) => {
-        if (results.response.entity == 'UPC') {
+        if (results && results.response.entity == 'UPC') {
             session.beginDialog('UPC');
         } else session.beginDialog('ETSETB');
     },
@@ -142,6 +205,8 @@ bot.dialog('menu', [
     }
 ]).triggerAction({
     matches: /^menu$/i
+}).cancelAction('cancelUPC', "Ok, pararé el dialeg UPC.", {
+    matches: /^cancelar$|^cancel$|^cancel.*order/i
 });
 
 bot.dialog('UPC', [
